b433ac89bf74b881017dff95cdf77aba64b7e05e
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-02-10 17:09:37 -0500

    Altered the import statement for the delclarative base class in the file database_setup.py.

--- vagrant/ProduceCatalog/databasesetup.py
+++ vagrant/ProduceCatalog/databasesetup.py
@@ -2,8 +2,7 @@ import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 
-from sqlalchemy.ext.declarative import
-declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import relationship
 
